96cc66729af4ade5dee46c6f695e078f5a71905d
John Karasinski <karasinski@gmail.com>
2013-05-28 19:54:03 -0700

    Make a baser User model (including secure passwords)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 SampleApp::Application.config.secret_token = '21d47e921962b8bf65a6cbbe5ae4c70c45b1b61350c7de40866261d28f8686faa8aa7852ef68b00ef38ebf1bd51cf760f3e83e4a603bdfdecc0cb9e439b3523e'
+SampleApp::Application.config.secret_key_base = 'df25ae79d5d3af52000db6d20e4770b784d4cca0bae121dcff422c87a89e81f0e0c2908ccb57c31e
+9f374bad0045c25abef5177aec06abeda2e6f66a559ce765'
